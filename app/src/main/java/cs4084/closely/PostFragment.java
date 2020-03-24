61c4519ddb323f5385a6ff70d56523fac6f3b559
Doc1998 <docmp98@gmail.com>
2020-03-24 17:54:59 +0000

    Blog page

--- app/src/main/java/cs4084/closely/PostFragment.java
+++ app/src/main/java/cs4084/closely/PostFragment.java
@@ -1,5 +1,6 @@
 package cs4084.closely;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
@@ -15,6 +18,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class PostFragment extends Fragment {
+    private onPostbutton listener;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -58,7 +62,21 @@ public class PostFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.post_fragment,
+                container, false);
+        Button post = getView().findViewById(R.id.postbutton);
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onPosted();
+
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.post_fragment, container, false);
+        return view;
+    }
+    public interface onPostbutton{
+        public void onPosted();
+
     }
 }
