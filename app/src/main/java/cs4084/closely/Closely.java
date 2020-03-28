7c8bf14cf3c6531db5b194bf9ffc8c0f4b9dc2dd
RonanQuill <17040434@studentmail.ul.ie>
2020-03-28 20:15:51 +0000

    Made new main activity Merged everything together

---
+++ app/src/main/java/cs4084/closely/Closely.java
@@ -0,0 +1,14 @@
+package cs4084.closely;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Closely extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_closely);
+    }
+}
