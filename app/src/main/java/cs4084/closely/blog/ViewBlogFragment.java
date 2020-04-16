26f02a8cb412b7cf7c0b0e37f197eee5f87ec86d
Doc1998 <docmp98@gmail.com>
2020-04-16 15:55:51 +0100

    Removed camera roll check/photos from commenting

--- app/src/main/java/cs4084/closely/blog/ViewBlogFragment.java
+++ app/src/main/java/cs4084/closely/blog/ViewBlogFragment.java
@@ -73,55 +73,10 @@ public class ViewBlogFragment extends Fragment {
         TextView bodyView = view.findViewById(R.id.view_blog_body);
         Button postComment = view.findViewById(R.id.view_blog_add_comment);
         final ImageView imageView =  view.findViewById(R.id.imageView2);
-        ImageButton imageBtn = view.findViewById(R.id.imageButton);
         postComment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getUsername();
-            }
-        });
-        imageBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (ContextCompat.checkSelfPermission(getActivity(),
-                            Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
-                        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
-                        requestPermissions(permissions, PERMISSION_CODE);
-
-                    } else {
-                        pickImageFromGallery();
-                    }
-                }
-            }
-            private void pickImageFromGallery() {
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                startActivityForResult(intent, IMAGE_PICK_CODE);
-            }
-
-
-            public void onActivityResult(int requestCode, int resultCode, Intent data) {
-                if(resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE){
-                    imageView.setImageURI(data.getData());
-                    Log.d(TAG, "happened");
-                }
-            }
-
-
-            public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-                switch (requestCode){
-                    case PERMISSION_CODE : {
-                        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                            pickImageFromGallery();
-                            Log.d(TAG, "happened");
-                        }
-                        else {
-                            Toast.makeText(getActivity(), "Permission DENIED",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                }
             }
         });
 
