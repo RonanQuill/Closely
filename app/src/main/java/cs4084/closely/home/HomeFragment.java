4f7313a54e03d45a5469b6e761d1dfae52657b79
rbrady98 <rian.brady71198@gmail.com>
2020-05-06 19:37:16 +0100

    Custom Image toggle for turning nfc on/off

--- app/src/main/java/cs4084/closely/home/HomeFragment.java
+++ app/src/main/java/cs4084/closely/home/HomeFragment.java
@@ -1,14 +1,15 @@
 package cs4084.closely.home;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CompoundButton;
-import android.widget.ToggleButton;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
 
 import cs4084.closely.Closely;
 import cs4084.closely.R;
@@ -25,19 +26,47 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        ToggleButton nfcToggleButton = view.findViewById(R.id.nfcToggleButton);
-        nfcToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                Closely closely = (Closely) getActivity();
-                if (isChecked) {
-                    // The toggle is enabled
+        final ImageView i = view.findViewById(R.id.home_image);
+        Animation rotate = AnimationUtils.loadAnimation(getContext(), R.anim.spin);
+        Animation pulse = AnimationUtils.loadAnimation(getContext(), R.anim.pulse);
+        final AnimationSet animationSet = new AnimationSet(false);
+        animationSet.addAnimation(rotate);
+        animationSet.addAnimation(pulse);
+        rotate.setRepeatMode(Animation.INFINITE);
+        pulse.setRepeatMode(Animation.REVERSE);
+
+        i.setOnClickListener(new View.OnClickListener() {
+            boolean nfcOn = false;
+            Closely closely = (Closely) getActivity();
+
+            @Override
+            public void onClick(View v) {
+                nfcOn = !nfcOn;
+                if (nfcOn) {
+                    i.startAnimation(animationSet);
                     closely.getNfcManager().enableNFC();
+
                 } else {
-                    // The toggle is disabled
+                    i.clearAnimation();
                     closely.getNfcManager().disableNFC();
                 }
+
             }
         });
+
+//        ToggleButton nfcToggleButton = view.findViewById(R.id.nfcToggleButton);
+//        nfcToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//
+//                if (isChecked) {
+//                    // The toggle is enabled
+//
+//                } else {
+//                    // The toggle is disabled
+//
+//                }
+//            }
+//        });
         return view;
     }
 }
